f17139b2200be7cebf5f31bffaea1f256e5be40c
joe <burnett.joe@gmail.com>
2019-07-02 19:41:53 -0400

    checking in

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const app = express()
 var PORT = process.env.PORT || 3000;
 
-router.get('/',(req,res) => {
+router.get('/', (req,res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'))
 })
 
