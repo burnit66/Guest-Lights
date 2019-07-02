e1607d7270a2649655f9afd9362c4f21e0677139
joe <burnett.joe@gmail.com>
2019-07-02 19:09:52 -0400

    adding views and serving html for index

--- auth/lightify.js
+++ auth/lightify.js
@@ -8,23 +8,24 @@ const options = {
     'path': '/authorize?client_id=' + process.env.CLIENT_ID + '&state={{state}}&redirect_uri=' + process.env.REDIRECT_URI + '&response_type=code',
     'headers': {
     }
-  }
-  
-  const req = https.request(options, function (res) {
-      const chunks = []
-  
-      res.on("data", function (chunk) {
-          chunks.push(chunk)
-      })
-  
-      res.on("end", function (chunk) {
-          const body = Buffer.concat(chunks)
-          console.log(body.toString())
-      })
-  
-      res.on("error", function (error) {
-          console.error(error)
-      })
-  })
-  
-  req.end()
+}
+
+
+const req = https.request(options, function (res) {
+    const chunks = []
+
+    res.on("data", function (chunk) {
+        chunks.push(chunk)
+    })
+
+    res.on("end", function (chunk) {
+        const body = Buffer.concat(chunks)
+        console.log(body.toString())
+    })
+
+    res.on("error", function (error) {
+        console.error(error)
+    })
+})
+
+req.end()
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const path = require('path')
+const router = express.Router()
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+router.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname + '/views/index.html'))
+})
 
+app.use('/', router)
 app.listen(port, () => console.log(`App listening on port ${port}!`))
