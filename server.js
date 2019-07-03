d527767afbc1b4d0b6bc9424d476531d698ceb6c
joe <burnett.joe@gmail.com>
2019-07-02 23:43:45 -0400

    refactoring

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ const express = require('express')
 const path = require('path')
 const router = express.Router()
 const app = express()
-const passport = require('passport')
-const passportSetup = require('./config/passport-setup')
 var PORT = process.env.PORT || 3000
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'))
 })
-
-router.get('/auth/lights', passport.authenticate('oauth', {
-    scope: []
-}))
 
 app.use('/', router)
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
